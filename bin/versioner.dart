e929a8ca312431020b71fff3f8a06d1bc00f86df
mayorbyrne <kmoritz@gmail.com>
2024-09-27 22:49:41 -0500

    chore: overwrite pubspec.yaml

--- bin/versioner.dart
+++ bin/versioner.dart
@@ -3,19 +3,14 @@ import 'package:pub_semver/pub_semver.dart';
 import 'dart:io';
 
 void main(List<String> args) {
-
   File('./pubspec.yaml').readAsString().then((String contents) {
     Pubspec pubspec = Pubspec.parse(contents);
-
     Version version = pubspec.version ?? Version(0, 0, 0);
-
     String incType = (args.isEmpty ? "major" : args[0]).toLowerCase();
 
     int major = version.major;
     int minor = version.minor;
     int patch = version.patch;
-
-    print("old version: $version");
 
     if (incType == "major") {
       ++major;
@@ -23,10 +18,14 @@ void main(List<String> args) {
       ++minor;
     } else if (incType == "patch") {
       ++patch;
-    } else if (incType == "actual version") {
+    } else {
       //TODO: create a new version type given the provided version
     }
 
-    print("new version: ${Version(major, minor, patch)}");
+    contents = contents.replaceAll("version: $version", "version: $major.$minor.$patch");
+
+    File('./pubspec.yaml').writeAsStringSync(contents);
+
+    print("v${Version(major, minor, patch)}");
   });
 }
